c19917357509051f8f1c363853b8e2e502d3d3c1
Ankit Sinha <ankits0057@gmail.com>
2026-02-07 17:31:02 +0530

    added all CRUD operation

--- server/resolvers/resolver.js
+++ server/resolvers/resolver.js
@@ -1,17 +1,34 @@
 import Todo from "../models/todoModel.js";
 
 const resolvers = {
-    Query:{
-        todos: async () => {
-            return await Todo.find();
-        }
+  Query: {
+    todos: async () => {
+      return await Todo.find();
     },
-    Mutation:{
-        addTodo: async (_, {title}) => {
-            const newTodo  = Todo.create({title});
-            return newTodo;
-        }
+  },
+  Mutation: {
+    addTodo: async (_, { data }) => {
+      const todo = new Todo({
+        title: data.title,
+      });
+      return await todo.save();
+    },toggleTodo: async (_, { id }) => {
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      throw new Error("Todo not found");
     }
-}
+    todo.completed = !todo.completed;
+    return await todo.save();
+  },
+    deleteTodo: async (_, { id }) => {
+      const todo = await Todo.findByIdAndDelete(id);
+      if (!todo) {
+        throw new Error("Todo not found");
+      }
+      return todo;
+    }
+  },
+  
+};
 
 export default resolvers;
